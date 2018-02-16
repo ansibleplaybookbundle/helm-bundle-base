015dbf05656b17d9bb86fa05bff948a3d65fced9
David Zager <david.j.zager@gmail.com>
2018-02-16 15:42:35 -0500

    Play with generic helm runner

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,18 +24,25 @@ fi
 ACTION=$1
 if [[ $ACTION == provision ]]; then
     KUBECTL_COMMAND=create
+    HELM_COMMAND=install
 elif [[ $ACTION == deprovision ]]; then
     KUBECTL_COMMAND=delete
+    HELM_COMMAND=delete
 else
     echo First argument must be one of "provision" or "deprovision".
     exit 1
 fi
 
 shift
-TARGET_NAMESPACE=$(echo "$2" | jq -r .namespace)
-INSTANCE_ID=$(echo "$2" | jq -r ._apb_service_instance_id)
+# Important variables
+TARGET_NAMESPACE=$(echo $2 | jq -r .namespace)
+REPO_URL=$(echo $2 | jq -r .repo)
+REPO_NAME=$(echo $2 | jq -r .repo_name)
+CHART=$(echo $2 | jq -r .chart)
+NAME=$(echo $2 | jq -r .name)
 VALUES_FILE=$(mktemp --tmpdir= values.XXXX)
 echo "$2" | jq -r .values | tee $VALUES_FILE
+[ "$(echo $2 | jq -r .tiller)" == "false" ] && USE_TILLER=0 || USE_TILLER=1
 
 if ! whoami &> /dev/null; then
   if [ -w /etc/passwd ]; then
@@ -43,14 +50,52 @@ if ! whoami &> /dev/null; then
   fi
 fi
 
+function setup_tiller() {
+    # Create permissions for tiller in target namespace
+    kubectl get serviceaccount --namespace=$TARGET_NAMESPACE tiller || \
+        kubectl create serviceaccount --namespace=$TARGET_NAMESPACE tiller
+    kubectl get rolebinding --namespace=$TARGET_NAMESPACE tiller || \
+        kubectl create rolebinding tiller \
+            --namespace=$TARGET_NAMESPACE \
+            --clusterrole=edit \
+            --serviceaccount=$TARGET_NAMESPACE:tiller
+    # Start Tiller
+    helm init --service-account=tiller --tiller-namespace=$TARGET_NAMESPACE
+}
+
+function teardown_tiller() {
+    helm reset --tiller-namespace=$TARGET_NAMESPACE && REMOVE_TILLER=1 || REMOVE_TILLER=0
+    if $REMOVE_TILLER; then
+        kubectl delete serviceaccount --namespace=$TARGET_NAMESPACE tiller
+        kubectl delete rolebinding --namespace=$TARGET_NAMESPACE tiller
+    fi
+}
+
 ### HELM
+if $USE_TILLER; then
+    setup_tiller
+fi
 
-helm template --debug --name bundle-${INSTANCE_ID::8} -f $VALUES_FILE /opt/chart.tgz | sed -n '/---/,$p' > /tmp/manifest
-echo "##########################"
-cat /tmp/manifest
-echo "##########################"
-kubectl $KUBECTL_COMMAND -n $TARGET_NAMESPACE -f /tmp/manifest
+helm repo add $REPO_NAME $REPO_URL
 
+if $USE_TILLER; then
+    helm $HELM_COMMAND $REPO_NAME/$CHART \
+        --tiller-namespace=$TARGET_NAMESPACE \
+        --name=$NAME \
+        --values=$VALUES_FILE \
+        --namespace=$TARGET_NAMESPACE
+else
+    helm fetch $REPO_NAME/$CHART --untar -d /opt/apb
+	helm template --name=$NAME /opt/apb/$CHART | sed -n '/---/,$p' > /tmp/manifest
+	echo "##########################"
+	cat /tmp/manifest
+	echo "##########################"
+	kubectl $KUBECTL_COMMAND --namespace=$TARGET_NAMESPACE -f /tmp/manifest
+fi
+
+if [[ $ACTION == deprovision ]]; then
+    teardown_tiller
+fi
 ###
 
 EXIT_CODE=$?
